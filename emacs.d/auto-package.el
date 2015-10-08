37d7b88f9e70448d92fd6d7ebc322bf21f56950e
hell@world <whispermemory@gmail.com>
2015-10-08 21:18:30 +0800

    Update auto-package.el

--- emacs.d/auto-package.el
+++ emacs.d/auto-package.el
@@ -1,46 +1,42 @@
-
 (require 'cl)
 (require 'package)
 
 (add-to-list 'package-archives'
-	     ("elpa" . "http://tromey.com/elpa/") t)
+             ("elpa" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives'
-	     ("marmalade" . "http://marmalade-repo.org/packages/") t)
+             ("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives'
-	     ("melpa" . "http://melpa.milkbox.net/packages/") t)
+             ("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-;; auto installed package                                                           
+;; auto installed package
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; setting default package to be installed                                          
+;; setting default package to be installed
 (defvar my-default-packages '(expand-region
-			      ;;                              evil
-			      go-mode
+                              evil
+			      pos-tip
+			      popup
                               auto-complete
-			      go-autocomplete
                               yasnippet
                               autopair
                               markdown-mode
                               xcscope
                               ace-jump-mode
-			      go-eldoc
+                              go-mode
+                           ;;   gist
+                              company
+			      rust-mode
 			      helm
-			      pos-tip
-			      popup
-			      fuzzy
-			      dired-details+
-			      jedi
-			      
                               ))
 (dolist (p my-default-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-;; normal useful variable set                                                       
-;; normal useful variable set                                                       
-(setq dotfiles-dir "~/.emacs.d");;这个我是在.emacs.d/init.el里定义的                
+;; normal useful variable set
+;; normal useful variable set
+(setq dotfiles-dir "~/.emacs.d");;
 (setq autoload-file (concat dotfiles-dir "/loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "/elpa"))
 (setq custom-file (concat dotfiles-dir "/custom.el"))
